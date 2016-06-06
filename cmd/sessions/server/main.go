b0f8d15026aa9f9f866f46149123be640c1bfcb6
Sergey Maksimov <sergey.maximov@ncloudtech.ru>
2016-06-06 20:30:31 +0300

    enable tracing

--- cmd/sessions/server/main.go
+++ cmd/sessions/server/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net"
+	"net/http"
 
 	"github.com/konjoot/grpc/services/sessions"
 	"google.golang.org/grpc"
@@ -14,11 +16,21 @@ const (
 	port = ":50051"
 )
 
+var trace = flag.Bool("trace", false, "Whether tracing is on")
+
 func main() {
+	flag.Parse()
+	grpc.EnableTracing = *trace
+
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+
+	if grpc.EnableTracing {
+		go http.ListenAndServe("localhost:34567", nil)
+	}
+
 	s := grpc.NewServer()
 	pb.RegisterSessionServer(s, sessions.New())
 	s.Serve(lis)
